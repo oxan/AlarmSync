e4cd529be3cec6e12152cbe1c6afb22227f35215
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2018-12-29 15:42:09 +0100

    Some clean-up & cruft removal (get rid of support library)

--- app/src/main/java/nl/oxanvanleeuwen/android/alarmsync/MainActivity.java
+++ app/src/main/java/nl/oxanvanleeuwen/android/alarmsync/MainActivity.java
@@ -1,10 +1,9 @@
 package nl.oxanvanleeuwen.android.alarmsync;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
